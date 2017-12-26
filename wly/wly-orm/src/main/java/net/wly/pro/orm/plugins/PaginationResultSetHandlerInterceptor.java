6dbcb1ea5427c5d6d51f2e68de977104eda3876f
tianlin <275012086@qq.com>
2017-12-26 16:03:34 +0800

    修改数据源+缓存

--- wly/wly-orm/src/main/java/net/wly/pro/orm/plugins/PaginationResultSetHandlerInterceptor.java
+++ wly/wly-orm/src/main/java/net/wly/pro/orm/plugins/PaginationResultSetHandlerInterceptor.java
@@ -1,23 +1,20 @@
 package net.wly.pro.orm.plugins;
 
-import java.sql.Statement;
-import java.util.Properties;
-
-import org.apache.ibatis.executor.resultset.FastResultSetHandler;
+import net.wly.pro.common.entity.Page;
+import org.apache.ibatis.executor.resultset.DefaultResultSetHandler;
 import org.apache.ibatis.executor.resultset.ResultSetHandler;
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.plugin.Intercepts;
-import org.apache.ibatis.plugin.Invocation;
-import org.apache.ibatis.plugin.Plugin;
-import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
 import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.ReflectorFactory;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.apache.ibatis.reflection.factory.ObjectFactory;
 import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
 import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
 import org.apache.ibatis.session.RowBounds;
 
-import net.wly.pro.common.entity.Page;
+import java.sql.Statement;
+import java.util.Properties;
 
 /**
  * PaginationResultSetHandlerInterceptor
@@ -26,27 +23,32 @@ import net.wly.pro.common.entity.Page;
 @Intercepts({ @Signature(type = ResultSetHandler.class, method = "handleResultSets", args = { Statement.class }) })
 public class PaginationResultSetHandlerInterceptor implements Interceptor {
 
-    private static final ObjectFactory DEFAULT_OBJECT_FACTORY = new DefaultObjectFactory();
-    private static final ObjectWrapperFactory DEFAULT_OBJECT_WRAPPER_FACTORY = new DefaultObjectWrapperFactory();
+	private static final ObjectFactory DEFAULT_OBJECT_FACTORY = new DefaultObjectFactory();
+	private static final ObjectWrapperFactory DEFAULT_OBJECT_WRAPPER_FACTORY = new DefaultObjectWrapperFactory();
+	private static final ReflectorFactory DEFAULT_REFLECTOR_FACTORY = new DefaultReflectorFactory();
 
-    public Object intercept(Invocation invocation) throws Throwable {
-        FastResultSetHandler resultSetHandler = (FastResultSetHandler) invocation.getTarget();
-        MetaObject metaStatementHandler = MetaObject.forObject(resultSetHandler, DEFAULT_OBJECT_FACTORY, DEFAULT_OBJECT_WRAPPER_FACTORY);
-        RowBounds rowBounds = (RowBounds) metaStatementHandler.getValue("rowBounds");
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		DefaultResultSetHandler resultSetHandler = (DefaultResultSetHandler) invocation.getTarget();
+		MetaObject metaStatementHandler = MetaObject.forObject(resultSetHandler, DEFAULT_OBJECT_FACTORY,
+				DEFAULT_OBJECT_WRAPPER_FACTORY, DEFAULT_REFLECTOR_FACTORY);
+		RowBounds rowBounds = (RowBounds) metaStatementHandler.getValue("rowBounds");
 
-        Object result = invocation.proceed();
+		Object result = invocation.proceed();
 
-        if (rowBounds instanceof Page) {
-            metaStatementHandler.setValue("rowBounds.rows", result);
-        }
-        return result;
-    }
+		if (rowBounds instanceof Page) {
+			metaStatementHandler.setValue("rowBounds.rows", result);
+		}
+		return result;
+	}
 
-    public Object plugin(Object target) {
-        return Plugin.wrap(target, this);
-    }
+	@Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
 
-    public void setProperties(Properties properties) {
-    }
+	@Override
+	public void setProperties(Properties properties) {
+	}
 
 }
--- wly/wly-orm/src/main/java/net/wly/pro/orm/plugins/PaginationStatementHandlerInterceptor.java
+++ wly/wly-orm/src/main/java/net/wly/pro/orm/plugins/PaginationStatementHandlerInterceptor.java
@@ -13,7 +13,9 @@ import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
 import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.ReflectorFactory;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.apache.ibatis.reflection.factory.ObjectFactory;
 import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
@@ -31,78 +33,79 @@ import net.wly.pro.orm.dialect.DialectFactory;
  * PaginationStatementHandlerInterceptor
  *
  */
-@Intercepts({ @Signature(type = StatementHandler.class, method = "prepare", args = { Connection.class }) })
+@Intercepts({ @Signature(type = StatementHandler.class, method = "prepare", args = { Connection.class, Integer.class }) })
 public class PaginationStatementHandlerInterceptor implements Interceptor {
 
-    private final static Logger logger = LoggerFactory.getLogger(PaginationStatementHandlerInterceptor.class);
+	private final static Logger logger = LoggerFactory.getLogger(PaginationStatementHandlerInterceptor.class);
 
-    private static final ObjectFactory DEFAULT_OBJECT_FACTORY = new DefaultObjectFactory();
-    private static final ObjectWrapperFactory DEFAULT_OBJECT_WRAPPER_FACTORY = new DefaultObjectWrapperFactory();
+	private static final ObjectFactory DEFAULT_OBJECT_FACTORY = new DefaultObjectFactory();
+	private static final ObjectWrapperFactory DEFAULT_OBJECT_WRAPPER_FACTORY = new DefaultObjectWrapperFactory();
+	private static final ReflectorFactory DEFAULT_REFLECTOR_FACTORY = new DefaultReflectorFactory();
 
-    public Object intercept(Invocation invocation) throws Throwable {
-        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
-        ParameterHandler parameterHandler = statementHandler.getParameterHandler();
-        BoundSql boundSql = statementHandler.getBoundSql();
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+		ParameterHandler parameterHandler = statementHandler.getParameterHandler();
+		BoundSql boundSql = statementHandler.getBoundSql();
 
-        MetaObject metaStatementHandler = MetaObject.forObject(statementHandler, DEFAULT_OBJECT_FACTORY, DEFAULT_OBJECT_WRAPPER_FACTORY);
-        RowBounds rowBounds = (RowBounds) metaStatementHandler.getValue("delegate.rowBounds");
-        // 没有分页参数
-        if (rowBounds == null || rowBounds == RowBounds.DEFAULT) {
-            return invocation.proceed();
-        }
+		MetaObject metaStatementHandler = MetaObject.forObject(statementHandler, DEFAULT_OBJECT_FACTORY,
+				DEFAULT_OBJECT_WRAPPER_FACTORY, DEFAULT_REFLECTOR_FACTORY);
+		RowBounds rowBounds = (RowBounds) metaStatementHandler.getValue("delegate.rowBounds");
+		// 没有分页参数
+		if (rowBounds == null || rowBounds == RowBounds.DEFAULT) {
+			return invocation.proceed();
+		}
 
-        Configuration configuration = (Configuration) metaStatementHandler.getValue("delegate.configuration");
-        Dialect dialect = DialectFactory.buildDialect(configuration);
-        String originalSql = (String) metaStatementHandler.getValue("delegate.boundSql.sql");
-        // 获取总记录数
-        Page<?> page = (Page<?>) rowBounds;
-        String countSql = dialect.getCountString(originalSql);
-        Connection connection = (Connection) invocation.getArgs()[0];
-        int total = getTotal(parameterHandler, connection, countSql);
-        page.setTotal(total);
+		Configuration configuration = (Configuration) metaStatementHandler.getValue("delegate.configuration");
+		Dialect dialect = DialectFactory.buildDialect(configuration);
+		String originalSql = (String) metaStatementHandler.getValue("delegate.boundSql.sql");
+		// 获取总记录数
+		Page<?> page = (Page<?>) rowBounds;
+		String countSql = dialect.getCountString(originalSql);
+		Connection connection = (Connection) invocation.getArgs()[0];
+		int total = getTotal(parameterHandler, connection, countSql);
+		page.setTotal(total);
 
-        // 设置物理分页语句
-        metaStatementHandler.setValue("delegate.boundSql.sql", dialect.getLimitString(originalSql, page.getOffset(), page.getLimit()));
-        // 屏蔽mybatis原有分页
-        metaStatementHandler.setValue("delegate.rowBounds.offset", RowBounds.NO_ROW_OFFSET);
-        metaStatementHandler.setValue("delegate.rowBounds.limit", RowBounds.NO_ROW_LIMIT);
-        if (logger.isDebugEnabled()) {
-            logger.debug("分页SQL : " + boundSql.getSql());
-        }
-        return invocation.proceed();
-    }
+		// 设置物理分页语句
+		metaStatementHandler.setValue("delegate.boundSql.sql",
+				dialect.getLimitString(originalSql, page.getOffset(), page.getLimit()));
+		// 屏蔽mybatis原有分页
+		metaStatementHandler.setValue("delegate.rowBounds.offset", RowBounds.NO_ROW_OFFSET);
+		metaStatementHandler.setValue("delegate.rowBounds.limit", RowBounds.NO_ROW_LIMIT);
+		if (logger.isDebugEnabled()) {
+			logger.debug("分页SQL : " + boundSql.getSql());
+		}
+		return invocation.proceed();
+	}
 
-    public Object plugin(Object target) {
-        return Plugin.wrap(target, this);
-    }
+	@Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
 
-    public void setProperties(Properties properties) {
-    }
+	@Override
+	public void setProperties(Properties properties) {
+	}
 
-    /**
-     * 获取总计录
-     * 
-     * @param parameterHandler
-     * @param connection
-     * @param countSql
-     * @return
-     * @throws Exception
-     */
-    private int getTotal(ParameterHandler parameterHandler, Connection connection, String countSql) throws Exception {
-        // MetaObject metaStatementHandler =
-        // MetaObject.forObject(parameterHandler);
-        // Object parameterObject =
-        // metaStatementHandler.getValue("parameterObject");
-        // TODO 缓存具有相同SQL语句和参数的总数
-        PreparedStatement prepareStatement = connection.prepareStatement(countSql);
-        parameterHandler.setParameters(prepareStatement);
-        ResultSet rs = prepareStatement.executeQuery();
-        int count = 0;
-        if (rs.next()) {
-            count = rs.getInt(1);
-        }
-        rs.close();
-        prepareStatement.close();
-        return count;
-    }
+	/**
+	 * 获取总计录
+	 * 
+	 * @param parameterHandler
+	 * @param connection
+	 * @param countSql
+	 * @return
+	 * @throws Exception
+	 */
+	private int getTotal(ParameterHandler parameterHandler, Connection connection, String countSql) throws Exception {
+		PreparedStatement prepareStatement = connection.prepareStatement(countSql);
+		parameterHandler.setParameters(prepareStatement);
+		ResultSet rs = prepareStatement.executeQuery();
+		int count = 0;
+		if (rs.next()) {
+			count = rs.getInt(1);
+		}
+		rs.close();
+		prepareStatement.close();
+		return count;
+	}
 }
