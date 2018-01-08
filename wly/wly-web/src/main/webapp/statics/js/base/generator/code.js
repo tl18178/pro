eb4645e8ecd1349179ab7628179fb2f575f7c391
tianlin <275012086@qq.com>
2018-01-08 09:20:19 +0800

    修改模版方式

--- wly/wly-web/src/main/webapp/statics/js/base/generator/code.js
+++ wly/wly-web/src/main/webapp/statics/js/base/generator/code.js
@@ -1,5 +1,7 @@
 /**
+
  * 生成代码-代码生成器js
+
  */
 var vm = new Vue({
 	el : '#dpLTE',
@@ -14,6 +16,10 @@ var vm = new Vue({
 			if (!$('#form').Validform()) {
 				return false;
 			}
+			if (vm.generator.requestMapping == vm.generator.viewPath) {
+                dialogMsg('请求地址和页面模块地址不能相同！');
+                return false;
+            }
 			toUrl('sys/generator/code?tables=' + vm.generator.tables
 					+ '&module=' + vm.generator.module + '&requestMapping='
 					+ vm.generator.requestMapping + '&viewPath='
